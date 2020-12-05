5292af19fcde8f13c98070364da75448d9cd58be
alladin7000 <47221194+alladin7000@users.noreply.github.com>
2020-12-05 11:05:27 +0300

    00
    _

--- network/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User,Blogpost,Followingdata,Likes
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Blogpost)
-admin.site.register(Followingdata)
-admin.site.register(Likes)
